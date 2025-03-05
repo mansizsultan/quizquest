522f94707747e704531baf60ab612182f08f1fe0
mansizsultan <sultanibnumansiz@gmail.com>
2025-03-05 12:59:19 +0700

    CREATE get username

--- backend/quiz/urls.py
+++ backend/quiz/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AllQuizzesList, QuizDelete, QuizListCreate
+from .views import AllQuizzesList, QuizDelete, QuizListCreate, get_username
 
 urlpatterns = [
     path("quizzes/", AllQuizzesList.as_view(), name="all_quizzes"),
     path("quizzes/user/", QuizListCreate.as_view(), name="user_quizzes"),
     path("quizzes/delete/<int:pk>/", QuizDelete.as_view(), name="delete_quiz"),
+    path("get_username/", get_username, name="get_username"),
 ]
--- backend/quiz/views.py
+++ backend/quiz/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 from .models import Quiz
 from .serializers import UserSerializer, QuizSerializer
 
@@ -8,6 +10,11 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_username(request):
+    return Response({"username": request.user.username})
 
 class AllQuizzesList(generics.ListAPIView):
     serializer_class = QuizSerializer
@@ -25,7 +32,7 @@ class QuizListCreate(generics.ListCreateAPIView):
         return Quiz.objects.filter(author=user)
 
     def perform_create(self, serializer):
-        if serializer.is_valid:
+        if serializer.is_valid():
             serializer.save(author=self.request.user)
         else:
             print(serializer.errors)
